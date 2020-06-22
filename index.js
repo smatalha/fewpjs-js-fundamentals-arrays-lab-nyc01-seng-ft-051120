7bf87a8b03086e6de1ac6889f5cd26c1c1d411f3
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-06-22 00:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
-// const removeFirst = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
 prepend.unshift("Odie");
 removeLast.pop();
-// removeFirst.shift();
+removeFirst.shift();
